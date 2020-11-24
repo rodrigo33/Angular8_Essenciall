66ad0ef6bb98fe4f8210b9efeb296aac3ae28f02
rodrigo33 <rbarbosa.barbosa@gmail.com>
2020-11-24 10:58:45 -0300

    aplicado eventos data binding

--- course-manager/src/app/courses/course-list.component.ts
+++ course-manager/src/app/courses/course-list.component.ts
@@ -7,12 +7,27 @@ import { CourseService } from './courses.service';
     templateUrl: './course-list.component.html'
 })
 export class CourseListComponent implements OnInit{
-courses: Course[] = [];
+//coloco _ na minha variável aqui para que outros desenvolvedores percebam que 
+//quero essas variáveis apenas aqui
+filteredCourses: Course[] = [];
+_courses: Course[] = [];
+_filterBy: string;
 
 constructor(private courseService: CourseService){}
 
 ngOnInit(): void{
-    this.courses = this.courseService.retrieveAll();    
+    this._courses = this.courseService.retrieveAll();        
+    this.filteredCourses = this._courses;
+    }
+    //criando evento de input para o meu html
+    set filter(value: string){
+        this._filterBy = value
 
-}
+        this.filteredCourses = this._courses.filter((course: Course) => course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1)
+    }
+
+    //evento para atualizar o input, leitura
+    get filter(){
+        return this._filterBy;
+    }
 }
